103e438897525b3c380333dd05c55d2636c9d720
unknown <victordzns1@gmail.com>
2024-11-28 16:57:08 -0300

    Category

---
+++ src/main/java/com/victordzns/course/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.victordzns.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victordzns.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+	
+}
